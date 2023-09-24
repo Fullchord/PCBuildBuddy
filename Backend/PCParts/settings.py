c8e6ee9f09c31c5d064d41433ea8a593245ed4af
Ethan Klausmeyer <112035330+Fullchord@users.noreply.github.com>
2023-09-23 23:01:48 -0500

    Start of REST framework build

--- Backend/PCParts/settings.py
+++ Backend/PCParts/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'PCParts',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'PCParts.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
