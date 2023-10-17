1f5c825b249817b9e9d7adfd19e7f2cb70ddb6d7
hlmoore6 <79874974+hlmoore6@users.noreply.github.com>
2023-10-17 13:33:31 -0500

    Added color palette. Removed unnecessary import.

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -2,7 +2,6 @@ import "./index.css";
 import "../components/nav";
 import Nav from "../components/nav.jsx";
 import Button from "../components/button.jsx";
-import Footer from "../components/footer.jsx";
 
 const Index = () => {
 
