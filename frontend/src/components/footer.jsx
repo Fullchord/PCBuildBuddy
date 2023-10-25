cf94b3ee680f09a5c58ca5dcab382363789688be
hlmoore6 <79874974+hlmoore6@users.noreply.github.com>
2023-10-25 12:17:18 -0500

    Changed "class" to "className"

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -5,10 +5,10 @@ import { Link } from "react-router-dom";
 const Footer = () => {
     return (
         <footer className="footer">
-            <Link class="footer-link" to={`/`}>Home</Link> 
-            <Link class="footer-link" to={`/about`}>About</Link> 
-            <Link class="footer-link" to={`/build`}>Build</Link> 
-            <Link class="footer-link" to={`/contact`}>Contact</Link> 
+            <Link className="footer-link" to={`/`}>Home</Link> 
+            <Link className="footer-link" to={`/about`}>About</Link> 
+            <Link className="footer-link" to={`/build`}>Build</Link> 
+            <Link className="footer-link" to={`/contact`}>Contact</Link> 
             <p className="p">@2023 PCBuildBuddy</p>
         </footer>
     );
