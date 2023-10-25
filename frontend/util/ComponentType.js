f805d6261d93cc44b7ca03b2bbff98bb169e3161
hlmoore6 <79874974+hlmoore6@users.noreply.github.com>
2023-10-25 12:18:55 -0500

    Made basic component type enumeration. Made constructor for PcComponent.

---
+++ frontend/util/ComponentType.js
@@ -0,0 +1,11 @@
+
+const ComponentType = Object.freeze({
+    CPU: Symbol("CPU"),
+    COOLER: Symbol("COOLER"),
+    MEMORY: Symbol("MEMORY"),
+    TOWER: Symbol("TOWER"), 
+    GPU: Symbol("GPU"),
+    MOTHERBOARD: Symbol("MOTHERBOARD"),
+    PSU: Symbol("PSU"),
+    STORAGE: Symbol("STORAGE")
+});
--- frontend/util/pcComponent.js
+++ frontend/util/pcComponent.js
@@ -1,6 +1,12 @@
 
 export class PcComponent {
-    constructor() {
-
+    constructor(id, type, name, description, price, manufacturer, specs) {
+        this.id = id;
+        this.type = type;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.manufacturer = manufacturer;
+        this.specs = specs;
     }
 }
