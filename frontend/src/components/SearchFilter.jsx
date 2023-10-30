6bb887570dcfbbb090c4616ce39298ec3577aaba
hlmoore6 <79874974+hlmoore6@users.noreply.github.com>
2023-10-30 14:11:53 -0500

    Updated filter component to handle naming better. Added react-hook-form.

--- frontend/src/components/SearchFilter.jsx
+++ frontend/src/components/SearchFilter.jsx
@@ -1,5 +1,6 @@
 import "./SearchFilter.css";
 
+import { useForm } from "react-hook-form";
 import { ComponentType } from "../../util/ComponentType";
 import TemplatedFilter from "./TemplatedFilter";
 
@@ -23,26 +24,26 @@ const Search = () => {
 }
 
 const Filter = () => {
-    const onFilter = (e) => {
-        e.preventDefault();
-        console.log(e.target);
+    const { register , handleSubmit } = useForm();
+    const onFilter = (data) => {
+        console.log(data);
     }
 
     return (
-        <form id="filter-content" >
-            <TemplatedFilter category={ComponentType.CPU}/>
+        <form id="filter-content" onSubmit={handleSubmit(onFilter)}>
+            <TemplatedFilter category={ComponentType.CPU} register={register}/>
             <span>
                 <label>Price Range</label>
                 <span>
                     <label htmlFor="price-range-min">Min</label>
-                    <input type="number" min="0" id="price-range-min" name="price-range-min"/>
+                    <input {...register("price-range-min")} type="number" placeholder="0" min="0" id="price-range-min" name="price-range-min"/>
                 </span>
                 <span>
                     <label htmlFor="price-range-max">Max</label>
-                    <input type="number" min="0" id="price-range-max" name="price-range-max"/>
+                    <input {...register("price-range-max")} type="number" placeholder="9999" min="0" id="price-range-max" name="price-range-max"/>
                 </span>
             </span>
-            <button type="submit" id="filter-button" onClick={onFilter}>Filter</button>
+            <button type="submit" id="filter-button">Filter</button>
         </form>
     );
 }
--- frontend/src/components/TemplatedFilter.jsx
+++ frontend/src/components/TemplatedFilter.jsx
@@ -1,35 +1,35 @@
 import filterTemplates from "../../util/filtertemplates.json";
 import { ComponentType } from "../../util/ComponentType";
 
-const TemplatedFilter = ({category}) => {
-    if (category == undefined) return (<></>);
+const TemplatedFilter = ({category, register}) => {
+    if (category == undefined || register == undefined) return (<></>);
 
     const filterTemplate = FindFilterTemplate(category);
     let components = [];
 
     for (let option of filterTemplate) {
         const optionId = GenerateIdFromName(option.name);
-        const subComponents = GenerateInputComponent(option);
+        const subComponents = GenerateInputComponent(option, register);
         const component = 
             <span key={`${optionId}-fields`}>
                 {subComponents}
-            </span>
+            </span>;
         components.push(component);
     } 
 
     return (<>{components}</>);
 }
 
-export function FindFilterTemplate(componentType) {
+function FindFilterTemplate(componentType) {
     if (componentType == undefined || componentType == ComponentType.UNDEFINED) return;
 
     const template = filterTemplates.find((v) => ComponentType.fromStr(v.type) == componentType);
     return template.filterParams;
 }
 
-function GenerateInputComponent(option) {
-    const id = `${GenerateIdFromName(option.name)}-field`;
-
+function GenerateInputComponent(option, register) {
+    const optionId = GenerateIdFromName(option.name);
+    const id = `${optionId}-field`;
     switch(option.type) {
         case "checkbox":
         case "color":
@@ -46,7 +46,7 @@ function GenerateInputComponent(option) {
         case "week":
             return [
                 <label htmlFor={id} key={`${id}-label`}>{option.name}</label>,
-                <input type={option.type} id={id} name={id} key={id}/>
+                <input {...register(optionId)} type={option.type} id={id} name={id} key={id}/>
             ];
 
         case "radio":
@@ -54,10 +54,13 @@ function GenerateInputComponent(option) {
             for (let v of option.values) {
                 const subId = `${id}-${GenerateIdFromName(v)}`;
                 rbs.push(
-                    <span>
-                        <label htmlFor={subId} key={`${subId}-label`}>{v}</label>
-                        <input type={option.type} id={subId} name={id} key={subId}/>
-                    </span>
+                    <label htmlFor={subId} key={`${subId}-label`}>{v}</label>,
+                    <input {...register(optionId)} 
+                        type={option.type} 
+                        key={subId} 
+                        id={subId} 
+                        value={GenerateIdFromName(v)}
+                    />
                 );
             } 
             return [
@@ -68,24 +71,69 @@ function GenerateInputComponent(option) {
             let options = [];
             for (let o of option.values) {
                 const oId = `${id}-${GenerateIdFromName(o)}`;
-                options.push(<option key={oId} value={oId}>{o}</option>);
+
+                if (o == null) {
+                    options.push(
+                        <option key={oId} value="none">None</option>
+                    );
+                    continue;
+                }
+
+                options.push(<option key={oId} value={GenerateIdFromName(o)}>{o}</option>);
             }
 
             return [
                 <label htmlFor={id} key={`${id}-label`}>{option.name}</label>,
-                <select name={id} id={id} key={id}>{options}</select>
+                <select {...register(optionId)} id={id} key={id}>{options}</select>
             ];
         
-        case "range":
+        case "numeric-range":
+            const min = option.min;
+            const max = option.max;
+            let defaultMin = min;
+            let defaultMax = max;
+            if (min == undefined || max == undefined) return <>NUMERIC RANGE ERROR</>;
 
+            if (option.default != undefined && option.default.min != undefined) 
+                defaultMin = option.default.min;
+            if (option.default != undefined && option.default.max != undefined)
+                defaultMax = option.default.max;
+
+            return [
+                <label key={`${id}-label`}>{option.name}</label>,
+                <label htmlFor={`${id}-min`} id={`${id}-min-label`}>Min</label>,
+                <input {...register(`${optionId}-min`)} type="number" placeholder={min} id={`${id}-min`} min={min} max={max}/>,
+                <label htmlFor={`${id}-min`} id={`${id}-min-label`}>Max</label>,
+                <input {...register(`${optionId}-max`)} type="number" placeholder={max} id={`${id}-max`} min={min} max={max}/>
+            ];
     }
 
     return [];
 }
 
 function GenerateIdFromName(name) {
-    // if (name == undefined) return;
-    return name.toLowerCase().replace(" ", "-");
+    if (name == undefined || name == null) {
+        return "null";
+    }
+
+    let n = name.toString().toLowerCase().replace(" ", "-");
+    
+    let openP = n.indexOf("(");
+    while (openP != -1) {
+        const closeP = n.indexOf(")");
+        if (closeP == -1) {
+            console.error("Malformed name in filterTemplates.json. Check for parentheses.");
+            return "PARENTHESES-ERROR-IN-FILTER-TEMPLATE";
+        }
+
+        const n0 = n.slice(0, openP - 1);
+        const n1 = n.slice(closeP + 1, n.length);
+
+        n = n0 + n1;
+        openP = n.indexOf("(");
+    }
+
+    return n;
 }
 
 export default TemplatedFilter;
