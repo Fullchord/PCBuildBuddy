edee11fbdf3d270996a8374031c62d7b6779dae9
Hunter Moore <hlmoore6@mail.fhsu.edu>
2023-11-01 22:01:10 -0500

    Implemented component store.

--- frontend/redux/componentSlice.js
+++ frontend/redux/componentSlice.js
@@ -1,15 +1,101 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 import { PcComponent } from '../util/pcComponent';
+import { Component } from 'react';
+import { ComponentType } from '../util/ComponentType';
+
+const categoryOrder = [
+    ComponentType.CPU,
+    ComponentType.COOLER,
+    ComponentType.MOTHERBOARD,
+    ComponentType.MEMORY,
+    ComponentType.STORAGE,
+    ComponentType.GPU,
+    ComponentType.TOWER,
+    ComponentType.PSU,
+];
 
 const componentSlice = createSlice({
     name: 'components',
     initialState: {
-        components: []
+        selectedComponents: [],
+        availableComponents: [],
+        currentCategory: ComponentType.CPU
     },
     reducers: {
+        clearAvailable: (state) => {
+            state.availableComponents = [];
+        },
 
+        clearSelected: (state) => {
+            state.selectedComponents = [];
+        },
+
+        addAvailable: (state, action) => {
+            state.availableComponents.push(action.payload);
+        },
+
+        addSelected: (state, action) => {
+            state.selectedComponents.push(action.payload);
+        },
+
+        incrementCategory: (state) => {
+            const currIdx = categoryOrder.find(state.currentCategory);
+            if (currIdx == -1 || currIdx == (categoryOrder.length - 1)) {
+                console.error(`Error incrementing the current category in state for category ${ComponentType.getStringRep(state.currentCategory)}`);
+                state.currentCategory = ComponentType.UNDEFINED;
+                return;
+            }
+
+            state.currentCategory = categoryOrder[currIdx+1];
+        },
+
+        /**
+         * 
+         * @param {state} state 
+         * @param {action} action 
+         * @description This reducer resets the component state to the requested category.
+         * THIS WILL DESTROY DATA IN THE STORE
+         * @returns 
+         */
+        resetCurrentCategory: (state, action) => {
+            const targetCat = action.payload | categoryOrder[0];
+            const targetCatIdx = categoryOrder.find(targetCat);
+
+            if(targetCatIdx == -1) { // Check for invalid index
+                console.error("targetCatIdx was -1. Target category to reset to is invalid.");
+                return;
+            }
+
+            state.currentCategory = targetCat;
+            
+            for (const [index, c] of state.selectedComponents.entries()) {
+                // If the type on the component is undefined, then we will assume it needs removed
+                // Ideally we wouldn't need this, but just in case :)
+                if (c.type == undefined) {
+                    console.warn("Component type was undefined! Please ensure that all components have a \"type\" field!");
+                    state.selectedComponents.splice(index, 1);
+                    continue;
+                }
+
+                const componentOrderIndex = categoryOrder.find(c);
+                if (componentOrderIndex == -1) { // Check for invalid index
+                    console.error(`Component type was defined, but was not included in the categoryOrder. 
+                        Please ensure that the category order is correctly defined. Category Type ${ComponentType.getStringRep(c)}`)
+                    
+                    state.selectedComponents.splice(index, 1);
+                    continue;
+                }
+                
+                if(componentOrderIndex >= targetCatIdx) {
+                    state.selectedComponents.splice(index, 1);
+                }
+            }
+        }
     }
 });
+
+
+export const { clearAvailable, clearSelected, addAvailable, addSelected } = componentSlice.actions;
 
 export default componentSlice.reducer;
