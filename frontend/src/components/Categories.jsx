45dff1c3ee12157d449e993b12a372d4153d9f40
Ethan Klausmeyer <112035330+Fullchord@users.noreply.github.com>
2023-11-06 11:51:35 -0600

    category current cat functionality

--- frontend/src/components/Categories.jsx
+++ frontend/src/components/Categories.jsx
@@ -3,29 +3,37 @@ import { BsFillCpuFill, BsFan, BsMotherboardFill, BsMemory, BsFillDeviceHddFill,
 import { PiComputerTowerFill } from 'react-icons/pi';
 import { FaPlug } from 'react-icons/fa6';
 import { ComponentType } from "../../util/ComponentType";
+import { useGetCategoryType } from "../../hooks/ComponentStoreUtil"
 
 const Categories = () => {
-    
-    const categories = [
-        ComponentType.CPU,
-        ComponentType.COOLER,
-        ComponentType.MOTHERBOARD,
-        ComponentType.MEMORY,
-        ComponentType.STORAGE,
-        ComponentType.GPU,
-        ComponentType.TOWER,
-        ComponentType.PSU,
-    ];
+
+    const currentCategory = useGetCategoryType();
+    const currentCategories = getCategories(currentCategory);
 
     return (
         <div id="categories-content">
-            {categories.map((type) => {
+            {currentCategories.map((type) => {
                 return (
-                    <Category categoryType={type} isSelected={type == ComponentType.CPU} key={ComponentType.getStringRep(type)}/>
+                    <Category categoryType={type} isSelected={ type == currentCategory } key={ComponentType.getStringRep(type)}/>
                 );
             })}       
         </div>
     );
+}
+
+const getCategories = (categoryType) => {
+    const categories = [
+        ComponentType.CPU, // 0
+        ComponentType.COOLER, // 1
+        ComponentType.MOTHERBOARD, // 2
+        ComponentType.MEMORY, // 3
+        ComponentType.STORAGE, // 4
+        ComponentType.GPU, // 5
+        ComponentType.TOWER, // 6
+        ComponentType.PSU, // 7
+    ];
+    const index = categories.indexOf(categoryType);
+    return categories.slice(index, categories.length);
 }
 
 const Category = ({categoryType, isSelected}) => {
