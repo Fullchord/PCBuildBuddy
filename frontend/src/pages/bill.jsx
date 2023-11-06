dcfc91fab3793ba970a3e05fd94c91912b7265c6
Brayan Gomez <bjgomez2@mail.fhsu.edu>
2023-11-06 15:10:08 -0600

    Added grid display layout to Bill of materials page

--- frontend/src/pages/bill.jsx
+++ frontend/src/pages/bill.jsx
@@ -9,12 +9,15 @@ const Bill = () => {
     return (
         <main>
             <Header enableStickyHeader />
-            <div>
-                <div id="main-content">
+            <div id="main-content">
+                <div id="left-side">
+                    <div><AnchorButton onclick="window.print();">Print</AnchorButton></div>
+                    <div><AnchorButton to="/">New Build?</AnchorButton></div>
+                </div>
+                <div id="right-side">
+
                 </div>
             </div>
-            <div><AnchorButton onclick="window.print();">Print</AnchorButton></div>
-            <div><AnchorButton to="/">New Build?</AnchorButton></div>
             <Footer/>
         </main>
     );
