ec808ba0cb91f4253be05839be13dcc980350388
Brayan Gomez <bjgomez2@mail.fhsu.edu>
2023-11-29 11:22:17 -0600

    Finished the contact page

--- frontend/src/pages/contact.jsx
+++ frontend/src/pages/contact.jsx
@@ -10,6 +10,24 @@ const Contact = () => {
     return (
         <main>
             <Nav/>
+            <div id="center-contact-page">
+                <h1 id="contact-title">Contact Us</h1>
+                <p id="contact-p">
+                    The PC Build Buddy team makes great effort to ensure a quality experience. 
+                    We welcome you to send us any problems or concerns that you may be experiencing with our product. 
+                    We also welcome you to send us any business inqueries and proposals.
+                </p>
+                <div id="contact-grid">
+                    <div id="contact-customer">
+                        <h2 className="contact-customer-business-titles">Customer Support</h2>
+                        <span><a href="mailto:someone@example.com">support@pcbuildbuddy.com</a></span>
+                    </div>
+                    <div id="contact-business">
+                        <h2 className="contact-customer-business-titles">Business Inqueries</h2>
+                        <span><a href="mailto:someone@example.com">business@pcbuildbuddy.com</a></span>
+                    </div>
+                </div>
+            </div>
             <Footer/>
         </main>
     );
