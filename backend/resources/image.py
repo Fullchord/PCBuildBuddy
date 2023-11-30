302c0175685ca6b57855f4e29efc25a6acf231c5
EthanKlausmeyer <112035330+Fullchord@users.noreply.github.com>
2023-11-30 12:08:46 -0600

    Static file Server

---
+++ backend/resources/image.py
@@ -0,0 +1,19 @@
+from flask_restful import Resource
+from flask import url_for
+from flask import send_file
+
+import os
+
+class Image(Resource):
+    def remove_extension(directory):
+        return os.path.splitext(directory)[0]
+    def get(self, image):
+        # return url_for('static', filename='i9-14900k.jpg')
+
+        directory = "./static"
+
+        for dirpath, dirnames, filenames in os.walk(directory):
+            for file in filenames:
+                filename = Image.remove_extension(file)
+                if filename == image:
+                    return send_file(f"./static/{file}")
